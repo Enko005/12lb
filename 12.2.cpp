7c4f7218b205c0ae116fd04432c02356de5851ab
Enko005 <mxmgalai@gmail.com>
2024-12-03 08:54:48 +0200

    Add files via upload

---
+++ 12.2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string.h>
+#include "windows.h"
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    char S[100], S1[20], S2[20];
+
+    cout << "Введіть рядок S: ";
+    cin.getline(S, 100); 
+
+    cout << "Введіть рядок S1 (підрядок для заміни): ";
+    cin.getline(S1, 20); 
+
+    cout << "Введіть рядок S2 (рядок для заміни): ";
+    cin.getline(S2, 20); 
+
+    char* pos = strstr(S, S1);
+
+    if (pos != nullptr) {
+        
+        char result[100] = ""; 
+   
+        strncat_s(result, S, pos - S);
+
+        strcat_s(result, S2);
+
+        strcat_s(result, pos + strlen(S1));
+
+        cout << "Результат після заміни: " << result << endl;
+    }
+    else {
+        cout << "Рядок S1 не знайдено у рядку S." << endl;
+    }
+
+}
+
